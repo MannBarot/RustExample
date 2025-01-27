888649cbb7cb6905023af9f8df1640057a438211
Mann Barot <manbar@unc.edu>
2025-01-27 03:53:09 +0000

    Created a hello COMP423 Rust file using my tutorial

---
+++ hello/target/debug/deps/hello-18cbee9ec7939d5b.d
@@ -0,0 +1,5 @@
+/workspaces/RustExample/hello/target/debug/deps/hello-18cbee9ec7939d5b: src/main.rs
+
+/workspaces/RustExample/hello/target/debug/deps/hello-18cbee9ec7939d5b.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/workspaces/RustExample/hello/target/debug/hello: /workspaces/RustExample/hello/src/main.rs
